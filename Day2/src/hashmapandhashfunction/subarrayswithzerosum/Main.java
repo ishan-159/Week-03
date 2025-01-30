27d2b1ed07a4bab71a8b93d7d748925a61cc8c4a
ishan-159 <ishantiwari159@gmail.com>
2025-01-30 16:35:11 +0530

    I am done with subarrays with zero sum problem.

---
+++ Day2/src/hashmapandhashfunction/subarrayswithzerosum/Main.java
@@ -0,0 +1,11 @@
+package hashmapandhashfunction.subarrayswithzerosum;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, -3, 1, 6, -6, 3, -3};
+
+        // Call the function from ZeroSumSubarrays class
+        ZeroSumSubarrays.findZeroSumSubarrays(arr);
+    }
+}
+
