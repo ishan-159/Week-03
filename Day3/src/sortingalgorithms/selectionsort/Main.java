56a03bcf21b7e60d3a67f4b3c5242b7e8e5e3720
ishan-159 <ishantiwari159@gmail.com>
2025-02-01 12:40:28 +0530

    I am done with selection sort problem.

---
+++ Day3/src/sortingalgorithms/selectionsort/Main.java
@@ -0,0 +1,16 @@
+package sortingalgorithms.selectionsort;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] scores =  {82,74,65,94,85,73};
+
+        // Create an instance of SelectionSort and sort the scores
+        SelectionSort sorter = new SelectionSort();
+        sorter.sort(scores);
+
+        // Print the sorted scores
+        System.out.println("Sorted exam scores:");
+        sorter.printArray(scores);
+    }
+}
